de3163ec1edf71144883ae7b929644dc7ffe5c1b
itzsamr <samarathiban@gmail.com>
2024-05-12 08:13:37 +0530

    entity - commit

---
+++ CRS/lease.py
@@ -0,0 +1,8 @@
+class Lease:
+    def __init__(self, leaseID, vehicleID, customerID, startDate, endDate, leaseType):
+        self.leaseID = leaseID
+        self.vehicleID = vehicleID
+        self.customerID = customerID
+        self.startDate = startDate
+        self.endDate = endDate
+        self.leaseType = leaseType
