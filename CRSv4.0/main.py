97fc78822465092945366fc0267d7a830ed6133d
itzsamr <samarathiban@gmail.com>
2024-05-26 21:49:15 +0530

    "Refactored main.py: updated menu formatting, added specific exception handling, and modified table formatting to 'simple_outline'."

--- CRSv4.0/main.py
+++ CRSv4.0/main.py
@@ -1,6 +1,6 @@
 from dao.ICarLeaseRepositoryImpl import ICarLeaseRepositoryImpl
-from exception.myexceptions import *
-from pyodbc import *
+from exception.myexceptions import CustomerNotFoundException, VehicleNotFoundException
+from pyodbc import Error
 from tabulate import tabulate
 from datetime import datetime
 
@@ -11,7 +11,7 @@ class CarRentalSystem:
         car_lease_repository = ICarLeaseRepositoryImpl()
 
         while True:
-            print("\n=====|Main Menu:|=====")
+            print("\n===== Main Menu =====")
             print("1. Customer Management")
             print("2. Vehicle Management")
             print("3. Lease Management")
@@ -22,27 +22,22 @@ class CarRentalSystem:
 
             if choice == "1":
                 CarRentalSystem.customer_management(car_lease_repository)
-
             elif choice == "2":
                 CarRentalSystem.vehicle_management(car_lease_repository)
-
             elif choice == "3":
                 CarRentalSystem.lease_management(car_lease_repository)
-
             elif choice == "4":
                 CarRentalSystem.payment_handling(car_lease_repository)
-
             elif choice == "5":
                 print("Exiting program.")
                 break
-
             else:
                 print("Invalid choice. Please try again.")
 
     @staticmethod
     def customer_management(car_lease_repository):
         while True:
-            print("\n=====|Customer Management:|=====")
+            print("\n===== Customer Management =====")
             print("1. Add Customer")
             print("2. Remove Customer")
             print("3. List Customers")
@@ -53,19 +48,14 @@ class CarRentalSystem:
 
             if sub_choice == "1":
                 CarRentalSystem.add_customer(car_lease_repository)
-
             elif sub_choice == "2":
                 CarRentalSystem.remove_customer(car_lease_repository)
-
             elif sub_choice == "3":
                 CarRentalSystem.list_customers(car_lease_repository)
-
             elif sub_choice == "4":
                 CarRentalSystem.find_customer_by_id(car_lease_repository)
-
             elif sub_choice == "5":
                 break
-
             else:
                 print("Invalid choice. Please try again.")
 
@@ -81,16 +71,18 @@ class CarRentalSystem:
                 customer_id, first_name, last_name, email, phone_number
             )
             print("Customer added successfully.")
-        except Exception as e:
+        except Error as e:
             print(f"Error adding customer: {e}")
 
     @staticmethod
     def remove_customer(car_lease_repository):
         try:
-            customer_id = int(input("Enter customer ID to remove: "))
+            customer_id = input("Enter customer ID to remove: ")
             car_lease_repository.remove_customer(customer_id)
             print("Customer removed successfully.")
-        except Exception as e:
+        except CustomerNotFoundException as e:
+            print(f"Error: {e}")
+        except Error as e:
             print(f"Error removing customer: {e}")
 
     @staticmethod
@@ -99,11 +91,11 @@ class CarRentalSystem:
             customers = car_lease_repository.list_customers()
             if customers:
                 headers = customers[0].keys()
-                rows = [customer.values() for customer in customers]
-                print(tabulate(rows, headers=headers, tablefmt="grid"))
+                rows = [list(customer.values()) for customer in customers]
+                print(tabulate(rows, headers=headers, tablefmt="simple_outline"))
             else:
                 print("No customers found.")
-        except Exception as e:
+        except Error as e:
             print(f"Error listing customers: {e}")
 
     @staticmethod
@@ -128,16 +120,18 @@ class CarRentalSystem:
                         customer["phoneNumber"],
                     ]
                 ]
-                print(tabulate(table, headers=headers, tablefmt="grid"))
+                print(tabulate(table, headers=headers, tablefmt="simple_outline"))
             else:
                 print("Customer not found.")
         except CustomerNotFoundException as e:
             print(f"Error: {e}")
+        except Error as e:
+            print(f"Error finding customer: {e}")
 
     @staticmethod
     def vehicle_management(car_lease_repository):
         while True:
-            print("\n=====|Vehicle Management:|=====")
+            print("\n===== Vehicle Management =====")
             print("1. Add Vehicle")
             print("2. Remove Vehicle")
             print("3. List Available Cars")
@@ -149,22 +143,16 @@ class CarRentalSystem:
 
             if sub_choice == "1":
                 CarRentalSystem.add_car(car_lease_repository)
-
             elif sub_choice == "2":
                 CarRentalSystem.remove_car(car_lease_repository)
-
             elif sub_choice == "3":
                 CarRentalSystem.list_available_cars(car_lease_repository)
-
             elif sub_choice == "4":
                 CarRentalSystem.list_of_rented_cars(car_lease_repository)
-
             elif sub_choice == "5":
                 CarRentalSystem.find_car_by_id(car_lease_repository)
-
             elif sub_choice == "6":
                 break
-
             else:
                 print("Invalid choice. Please try again.")
 
@@ -190,7 +178,9 @@ class CarRentalSystem:
                 engine_capacity,
             )
             print("Vehicle added successfully.")
-        except Exception as e:
+        except ValueError:
+            print("Invalid input. Please enter valid data types.")
+        except Error as e:
             print(f"Error adding vehicle: {e}")
 
     @staticmethod
@@ -198,7 +188,10 @@ class CarRentalSystem:
         try:
             vehicle_id = input("Enter vehicle ID to remove: ")
             car_lease_repository.remove_car(vehicle_id)
-        except Exception as e:
+            print("Vehicle removed successfully.")
+        except VehicleNotFoundException as e:
+            print(f"Error: {e}")
+        except Error as e:
             print(f"Error removing vehicle: {e}")
 
     @staticmethod
@@ -208,10 +201,10 @@ class CarRentalSystem:
             if cars:
                 headers = cars[0].keys()
                 rows = [list(car.values()) for car in cars]
-                print(tabulate(rows, headers=headers, tablefmt="grid"))
+                print(tabulate(rows, headers=headers, tablefmt="simple_outline"))
             else:
                 print("No available cars found.")
-        except Exception as e:
+        except Error as e:
             print(f"Error listing available cars: {e}")
 
     @staticmethod
@@ -221,11 +214,11 @@ class CarRentalSystem:
             if cars:
                 headers = cars[0].keys()
                 rows = [list(car.values()) for car in cars]
-                print(tabulate(rows, headers=headers, tablefmt="grid"))
+                print(tabulate(rows, headers=headers, tablefmt="simple_outline"))
             else:
-                print("No available cars found.")
-        except Exception as e:
-            print(f"Error listing available cars: {e}")
+                print("No rented cars found.")
+        except Error as e:
+            print(f"Error listing rented cars: {e}")
 
     @staticmethod
     def find_car_by_id(car_lease_repository):
@@ -255,16 +248,18 @@ class CarRentalSystem:
                         car["engineCapacity"],
                     ]
                 ]
-                print(tabulate(table, headers=headers, tablefmt="grid"))
+                print(tabulate(table, headers=headers, tablefmt="simple_outline"))
             else:
                 print("Car not found.")
         except VehicleNotFoundException as e:
             print(f"Error: {e}")
+        except Error as e:
+            print(f"Error finding car: {e}")
 
     @staticmethod
     def lease_management(car_lease_repository):
         while True:
-            print("\n=====|Lease Management:|=====")
+            print("\n===== Lease Management =====")
             print("1. Create Lease")
             print("2. Return Car")
             print("3. List Active Leases")
@@ -275,19 +270,14 @@ class CarRentalSystem:
 
             if sub_choice == "1":
                 CarRentalSystem.create_lease(car_lease_repository)
-
             elif sub_choice == "2":
                 CarRentalSystem.return_car(car_lease_repository)
-
             elif sub_choice == "3":
                 CarRentalSystem.list_active_leases(car_lease_repository)
-
             elif sub_choice == "4":
                 CarRentalSystem.list_lease_history(car_lease_repository)
-
             elif sub_choice == "5":
                 break
-
             else:
                 print("Invalid choice. Please try again.")
 
@@ -303,7 +293,10 @@ class CarRentalSystem:
             car_lease_repository.create_lease(
                 lease_id, customer_id, car_id, start_date, end_date, lease_type
             )
-        except Exception as e:
+            print("Lease created successfully.")
+        except ValueError:
+            print("Invalid date format. Please use YYYY-MM-DD.")
+        except Error as e:
             print(f"Error creating lease: {e}")
 
     @staticmethod
@@ -311,7 +304,10 @@ class CarRentalSystem:
         try:
             lease_id = input("Enter lease ID to return the car: ")
             car_lease_repository.return_car(lease_id)
-        except Exception as e:
+            print("Car returned successfully.")
+        except VehicleNotFoundException as e:
+            print(f"Error: {e}")
+        except Error as e:
             print(f"Error returning car: {e}")
 
     @staticmethod
@@ -342,10 +338,10 @@ class CarRentalSystem:
                     ]
                     for lease in leases
                 ]
-                print(tabulate(rows, headers=headers, tablefmt="grid"))
+                print(tabulate(rows, headers=headers, tablefmt="simple_outline"))
             else:
                 print("No active leases found.")
-        except Exception as e:
+        except Error as e:
             print(f"Error listing active leases: {e}")
 
     @staticmethod
@@ -372,16 +368,16 @@ class CarRentalSystem:
                     ]
                     for lease in leases
                 ]
-                print(tabulate(rows, headers=headers, tablefmt="grid"))
+                print(tabulate(rows, headers=headers, tablefmt="simple_outline"))
             else:
                 print("No lease history found.")
-        except Exception as e:
+        except Error as e:
             print(f"Error listing lease history: {e}")
 
     @staticmethod
     def payment_handling(car_lease_repository):
         while True:
-            print("\n=====|Payment Handling:|=====")
+            print("\n===== Payment Handling =====")
             print("1. Record Payment")
             print("2. Retrieve Payment History")
             print("3. Calculate Total Revenue")
@@ -392,19 +388,14 @@ class CarRentalSystem:
 
             if sub_choice == "1":
                 CarRentalSystem.record_payment(car_lease_repository)
-
             elif sub_choice == "2":
                 CarRentalSystem.retrieve_payment_history(car_lease_repository)
-
             elif sub_choice == "3":
                 CarRentalSystem.calculate_total_revenue(car_lease_repository)
-
             elif sub_choice == "4":
                 CarRentalSystem.list_all_payments(car_lease_repository)
-
             elif sub_choice == "5":
                 break
-
             else:
                 print("Invalid choice. Please try again.")
 
@@ -425,7 +416,7 @@ class CarRentalSystem:
             print(
                 "Invalid input. Please enter valid lease ID, payment date, and payment amount."
             )
-        except Exception as e:
+        except Error as e:
             print(f"Error recording payment: {e}")
 
     @staticmethod
@@ -444,19 +435,23 @@ class CarRentalSystem:
                     ]
                     for payment in payments
                 ]
-                print(tabulate(rows, headers=headers, tablefmt="grid"))
+                print(tabulate(rows, headers=headers, tablefmt="simple_outline"))
             else:
                 print("No payment history found for the given lease ID.")
         except ValueError:
             print("Invalid input. Please enter a valid lease ID.")
-        except Exception as e:
+        except Error as e:
             print(f"Error retrieving payment history: {e}")
 
     @staticmethod
     def calculate_total_revenue(car_lease_repository):
         try:
             total_revenue = car_lease_repository.calculate_total_revenue()
-            print(tabulate([[f"Total Revenue: {total_revenue}"]], tablefmt="grid"))
+            print(
+                tabulate(
+                    [[f"Total Revenue: {total_revenue}"]], tablefmt="simple_outline"
+                )
+            )
         except Exception as e:
             print(f"Error calculating total revenue: {e}")
 
@@ -466,11 +461,11 @@ class CarRentalSystem:
             payments = car_lease_repository.list_all_payments()
             if payments:
                 headers = payments[0].keys()
-                rows = [payment.values() for payment in payments]
-                print(tabulate(rows, headers=headers, tablefmt="grid"))
+                rows = [list(payment.values()) for payment in payments]
+                print(tabulate(rows, headers=headers, tablefmt="simple_outline"))
             else:
                 print("No payments found.")
-        except Exception as e:
+        except Error as e:
             print(f"Error listing all payments: {e}")
 
 
